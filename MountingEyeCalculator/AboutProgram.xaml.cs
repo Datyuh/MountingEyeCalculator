c43f8faf88b194ac42ff982eac042da061598608
ogkuav <uchaev100@yandex.ru>
2021-04-29 11:40:02 +0300

    Добавьте файлы проекта.

---
+++ MountingEyeCalculator/AboutProgram.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace StudCalculator.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AboutProgram.xaml
+    /// </summary>
+    public partial class AboutProgram : Window
+    {
+        public AboutProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ MountingEyeCalculator/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MountingEyeCalculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Border_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
